73a29bfd9539294463e007f81de4c0b7ca035281
defineapoorv <apoorvagarwal00@gmail.com>
2022-02-23 12:03:16 +0530

    new build

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
             <Navbar/>
             <div className='name'>
                 <h1>
-                    <span>Understanding</span> Zoning data for any parcel
+                    <span>Simplified</span> Zoning data for any parcel
                 </h1>
                 <p className='details'> We are making life of Architects Easier</p>
                 <Link to='/tool' className='cv-btn'>Explore</Link>
