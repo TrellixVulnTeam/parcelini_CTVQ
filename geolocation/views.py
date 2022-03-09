ea413a72a7efc5dec5f9d5b7992b7e4b0ff7fb1b
defineapoorv <apoorvagarwal00@gmail.com>
2022-03-09 23:41:00 +0530

    remove bf

--- geolocation/views.py
+++ geolocation/views.py
@@ -4,7 +4,8 @@ from geolocation.hazards import get_seisimic_zone_ca_info
 from geolocation.hazards import get_fire_hazard_zones_ca
 from geolocation.hazards import get_fema_flood_zones_national
 from geolocation.hazards import get_floodplains_ca
-from geolocation.building_footprints import get_building_footprints
+
+# from geolocation.building_footprints import get_building_footprints
 from geolocation.addressing import get_slope_of_property
 from rest_framework.response import Response
 
@@ -23,11 +24,11 @@ class HillsideInfo(APIView):
         return Response(data)
 
 
-class MapboxBuildingFootprints(APIView):
-    def get(self, request, address_slug):
-        address = address_slug.replace("+", " ")
-        data = get_building_footprints(address)
-        return Response(data)
+# class MapboxBuildingFootprints(APIView):
+#     def get(self, request, address_slug):
+#         address = address_slug.replace("+", " ")
+#         data = get_building_footprints(address)
+#         return Response(data)
 
 
 class FloodPlainInfo(APIView):
