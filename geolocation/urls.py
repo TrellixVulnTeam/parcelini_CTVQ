059bb3e8fa12068c8c4641d5fb28beee99446d67
defineapoorv <apoorvagarwal00@gmail.com>
2022-03-16 22:49:43 +0530

    reqs

--- geolocation/urls.py
+++ geolocation/urls.py
@@ -5,10 +5,10 @@ from . import views
 urlpatterns = [
     path("api/get_seismic_info/<str:address_slug>", views.SeisimicInfo.as_view()),
     path("api/get_hillside_slope/<str:address_slug>", views.HillsideInfo.as_view()),
-    # path(
-    #     "api/get_mb_building_footprints/<str:address_slug>",
-    #     views.MapboxBuildingFootprints.as_view(),
-    # ),
+    path(
+        "api/get_mb_building_footprints/<str:address_slug>",
+        views.MapboxBuildingFootprints.as_view(),
+    ),
     path("api/get_flood_plain_info/<str:address_slug>", views.FloodPlainInfo.as_view()),
     path(
         "api/get_fema_flood_zones_info/<str:address_slug>",
--- geolocation/views.py
+++ geolocation/views.py
@@ -10,6 +10,10 @@ from geolocation.addressing import get_slope_of_property
 from rest_framework.response import Response
 
 
+import requests
+from rest_framework import status
+
+
 class SeisimicInfo(APIView):
     def get(self, request, address_slug):
         address = address_slug.replace("+", " ")
@@ -24,11 +28,18 @@ class HillsideInfo(APIView):
         return Response(data)
 
 
-# class MapboxBuildingFootprints(APIView):
-#     def get(self, request, address_slug):
-#         address = address_slug.replace("+", " ")
-#         data = get_building_footprints(address)
-#         return Response(data)
+class MapboxBuildingFootprints(APIView):
+    def get(self, request, address_slug):
+        address = address_slug.replace("+", " ")
+        r = requests.post(
+            "https://protected-peak-85531.herokuapp.com/get_mapbox_building_footprints",
+            params={"address": address},
+        )
+        if r.status_code == 200:
+            return Response(r.json())
+        else:
+            content = {"message": "Please use a correct California address"}
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
 
 
 class FloodPlainInfo(APIView):
