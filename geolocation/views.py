d3bf0ae6746c7b57e749c6fa17d9830e4dfebe9b
defineapoorv <apoorvagarwal00@gmail.com>
2022-04-04 22:19:13 +0530

    models

--- geolocation/views.py
+++ geolocation/views.py
@@ -5,8 +5,7 @@ from geolocation.hazards import get_fire_hazard_zones_ca
 from geolocation.hazards import get_fema_flood_zones_national
 from geolocation.hazards import get_floodplains_ca
 from django.contrib.auth.models import User
-
-# from accounts.models import UserHistory
+from accounts.models import UserHistory
 
 # from geolocation.building_footprints import get_building_footprints
 from geolocation.addressing import get_slope_of_property
